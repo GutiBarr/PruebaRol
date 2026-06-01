0488ae55984439ed175c7e62853f3439df0011f9
AlejandroBs-06 <alejandro05bs06@gmail.com>
2026-06-01 11:08:42 +0200

    Migración a base de datos local

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   port: parseInt(process.env.PG_PORT || '5432'),
   database: process.env.PG_DATABASE || 'postgres',
   user: process.env.PG_USER || 'postgres',
-  password: process.env.PG_PASSWORD,
+  password: 'stemdo',
 });
 
 pool.connect()
